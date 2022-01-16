ca51bd385aa9c1948ae032d6888c4a9d1ee7a6cc
Zinnia <zinnia2563@gmail.com>
2022-01-16 12:34:29 +0600

    branch related api completed

--- controllers/branchController.js
+++ controllers/branchController.js
@@ -1,7 +1,7 @@
 const asyncHandler = require("express-async-handler");
 const Branch = require("../models/branchModel");
 const axios = require('axios');
-//Login for Restaurant owner 
+//Login for Branch owner 
 const branchCreate = asyncHandler(async (req, res) => {
     const {
         Branch_name,
@@ -33,7 +33,44 @@ const branchCreate = asyncHandler(async (req, res) => {
     return res.status(400).json({ error: error.toString() });
    }
 });
+const getSingleBranch = asyncHandler(async(req,res)=> {
+    const branch = await Branch.findById(req.params.branch_id)
+    try {
+      res.status(200).json({
+        message: "successfully get branch data",
+        data: branch
+    })
+    } catch (error) {
+      return res.status(400).json({ error: error.toString() });
+    }
+})
+const updateBranch = asyncHandler(async(req,res) => {
+  const branch = await Branch.findById(req.params.branch_id)
+  const { Address } = req.body;
+  //calling external api for lat long
+  const response = await axios.get(`https://geocode.maps.co/search?q=${Address}`);
+  const data = response.data;
+  const Lat = data[0].lat
+  const Lng = data[0].lon;
 
+  if(branch){
+      branch.Res_id  = req.params.res_id || branch.Res_id
+      branch.Branch_name = req.body.Branch_name || branch.Branch_name
+      branch.Address = req.body.Address || branch.Address
+      branch.Lat = Lat || branch.Lat
+      branch.Lng = Lng || branch.Lat
+  }
+  try {
+      const responseData = await branch.save();
+      res.status(201).json({
+        message: "Branch updated successfully!",
+    })
+  } catch (error) {
+    return res.status(400).json({ error: error.toString() });
+  }
+})
 module.exports = {
- branchCreate
+ branchCreate,
+ getSingleBranch,
+ updateBranch
 };
--- routes/restaurantRouter.js
+++ routes/restaurantRouter.js
@@ -7,12 +7,18 @@ const {
  GetAllRestaurant,
  getSingleRestaurant
 } = require("../controllers/restaurantController");
-const{branchCreate} = require("../controllers/branchController");
+const{branchCreate,getSingleBranch,updateBranch} = require("../controllers/branchController");
 
+//restaurent related path
 router.route("/get_all").get(protect,GetAllRestaurant);
-router.route("/:id").get(protect,getSingleRestaurant);
-router.route("/:res_id/branch/create").post(protect,branchCreate)
 router.route("/create").post(protect,RestaurantCreate);
 router.route("/update/:id").patch(protect,RestaurantUpdate);
+router.route("/:id").get(protect,getSingleRestaurant);
+
+//branch related path
+router.route("/:res_id/branch/create").post(protect,branchCreate);
+router.route("/:res_id/branch/:branch_id").get(protect,getSingleBranch);
+router.route("/:res_id/branch/update/:branch_id").patch(protect,updateBranch)
+
 
 module.exports = router;
