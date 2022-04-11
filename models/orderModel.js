f46dc46d0bfdbab5844369783202f97ca8e32b7e
Zinnia <58551646+zinnia2563@users.noreply.github.com>
2022-04-11 23:45:03 +0600

    Update orderModel.js

--- models/orderModel.js
+++ models/orderModel.js
@@ -1,54 +1,53 @@
 const mongoose = require("mongoose");
 
-const orderitems =mongoose.Schema({
-    menu_id: {
-        type: mongoose.Schema.Types.ObjectId, 
-        required: false,
-        ref: 'Menu'
-     },
-     quantity: {
-         type: Number
-     },
-     item_name:{
-         type: String
-     }
-  });
+const orderitems = mongoose.Schema({
+  menu_id: {
+    type: mongoose.Schema.Types.ObjectId,
+    required: false,
+    ref: "Menu",
+  },
+  quantity: {
+    type: Number,
+  },
+  item_name: {
+    type: String,
+  },
+});
 
-const orderSchema = mongoose.Schema({
+const orderSchema = mongoose.Schema(
+  {
     Restaurant_id: {
-        type: mongoose.Schema.Types.ObjectId,
-        required: true,
-        ref: 'Restaurant',
-      },
-    Order_id: { 
-        type: mongoose.Schema.Types.ObjectId, 
-        required: false,
-        ref: 'Order'
+      type: mongoose.Schema.Types.ObjectId,
+      required: true,
+      ref: "Restaurant",
     },
-    Table_id: { 
-        type: mongoose.Schema.Types.ObjectId, 
-        required: false,
-        ref: 'Table'
+    Order_id: {
+      type: mongoose.Schema.Types.ObjectId,
+      required: false,
+      ref: "Order",
     },
-    orderItems: [
-       orderitems
-      ],
-    Accepted_at: {type: Date, required:false, default: null},
-    Completed_at: {type: Date, required: false, default: null},
-    Serving_time: {type : Date, default: Date.now},
+    Table_id: {
+      type: mongoose.Schema.Types.ObjectId,
+      required: false,
+      ref: "Table",
+    },
+    orderItems: [orderitems],
+    orderDate: {
+      type: String,
+    },
+    Accepted_at: { type: Date, required: false, default: null },
+    Completed_at: { type: Date, required: false, default: null },
+    Serving_time: { type: Date, default: Date.now },
     status: {
-        type: Boolean,
-        default: 0
+      type: Number,
+      default: 0,
     },
-    
-},
-{
-    timestamps: true
-})
+  },
+  {
+    timestamps: true,
+  }
+);
 
-const order = mongoose.model('orderSchema',orderSchema)
+const order = mongoose.model("orderSchema", orderSchema);
 
 module.exports = order;
-
-
-
